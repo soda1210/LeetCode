acb096babb09757434b0d11aa2dd35e455582a65
ytzhou <ytzhou1210@gmail.com>
2025-04-30 10:33:51 +0800

    refactor: 調整程式碼格式以增強可讀性

--- Easy/242_Valid_Anagram/main.cpp
+++ Easy/242_Valid_Anagram/main.cpp
@@ -3,22 +3,31 @@
 using namespace std;
 
 class Solution {
-  public:
-      bool isAnagram(string s, string t) {
-          unordered_map<char, int> hash;
-  
-          if (s.size() != t.size()) { return false; }
-  
-          for (int i = 0; i<s.size(); i++){
-              hash[s[i]]++;
-              hash[t[i]]--;
-          }
-          
-          for (const auto& target:hash){
-              if(target.second != 0){
-                  return false;
-              }
-          }
-          return true;
+ public:
+  bool isAnagram(string s, string t) {
+    unordered_map<char, int> hash;
+
+    if (s.size() != t.size()) {
+      return false;
+    }
+
+    for (int i = 0; i < s.size(); i++) {
+      hash[s[i]]++;
+      hash[t[i]]--;
+    }
+
+    for (const auto& target : hash) {
+      if (target.second != 0) {
+        return false;
       }
-  };
+    }
+    return true;
+  }
+};
+
+int main() {
+  string s = "anagram";
+  string t = "nagaram";
+  Solution sol;
+  cout << sol.isAnagram(s, t);
+}
