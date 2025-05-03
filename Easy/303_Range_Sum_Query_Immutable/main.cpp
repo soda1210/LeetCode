b0ab9b2feacf45274c76c3695a5bde88689c8ff7
ytzhou <ytzhou1210@gmail.com>
2025-05-03 23:16:18 +0800

    refactor: 調整 NumArray 類別的程式碼格式以增強可讀性

--- Easy/303_Range_Sum_Query_Immutable/main.cpp
+++ Easy/303_Range_Sum_Query_Immutable/main.cpp
@@ -3,24 +3,31 @@
 using namespace std;
 
 class NumArray {
-  private:
-      vector<int> prefix;
-  
-  public:
-      NumArray(vector<int>& nums) {
-          prefix.resize(nums.size() + 1, 0);
-          for (int i = 0; i<nums.size(); ++i){
-              prefix[i+1] = nums[i] + prefix[i];
-          }
-      }
-      
-      int sumRange(int left, int right) {
-          return prefix[right+1] - prefix[left];
-      }
-  };
-  
-  /**
-   * Your NumArray object will be instantiated and called as such:
-   * NumArray* obj = new NumArray(nums);
-   * int param_1 = obj->sumRange(left,right);
-   */
+ private:
+  vector<int> prefix;
+
+ public:
+  NumArray(vector<int>& nums) {
+    prefix.resize(nums.size() + 1, 0);
+    for (int i = 0; i < nums.size(); ++i) {
+      prefix[i + 1] = nums[i] + prefix[i];
+    }
+  }
+
+  int sumRange(int left, int right) { return prefix[right + 1] - prefix[left]; }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
+
+int main() {
+  vector<int> nums = {-2, 0, 3, -5, 2, -1};
+  NumArray obj(nums);
+  cout << obj.sumRange(0, 2) << endl;  // 1
+  cout << obj.sumRange(2, 5) << endl;  // -1
+  cout << obj.sumRange(0, 5) << endl;  // -3
+  return 0;
+}
